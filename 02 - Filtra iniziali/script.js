fcdda6d7d122eea5ae7d608bcb296a2f48401d32
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-07 15:07:33 +0200

    Add Filtra iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,32 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+
+function checkCharInArray(array, char) {
+    let newArr = []
+    for (let i = 0; i < array.length; i++) {
+        let string = array[i].slice("");
+
+        let firstChar = string[0]
+
+        if (firstChar === char) {
+            newArr.push(string)
+        }
+        
+
+        
+    }
+
+    return newArr
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(checkCharInArray(names, "A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
