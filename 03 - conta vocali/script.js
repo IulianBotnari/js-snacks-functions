16a27cbc39792bd5d0252c28adb316df58ad9fcb
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-07 15:41:30 +0200

    Add conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,6 +2,34 @@
 
 const word = 'javascript';
 
+function findVocal(params) {
+
+        let arrayChar = params.split("")
+        
+        let contatore = 0
+        
+        let vocalArray = ["a", "e", "i", "o", "u"]
+
+        for (let i = 0; i < arrayChar.length; i++){
+
+            let char = arrayChar[i]            
+
+            if (vocalArray.includes(char)) {
+                contatore++
+            }
+
+
+
+        }
+
+        return contatore
+        
+}
+
+let contaLeVocali = findVocal(word)
+
+console.log(contaLeVocali);
+
 
 // Dichiara la funzione qui.
 
