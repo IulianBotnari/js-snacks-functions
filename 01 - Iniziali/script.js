efdb47812b48755a6b55141c256b1a09d5fa0caf
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-07 14:55:38 +0200

    Add iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,8 +2,35 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+// console.log(names);
+
 
 // Dichiara la funzione qui.
+function selectName (params) {
+    let newArr = []
+    for (let i = 0; i < params.length; i++) {    
+        let name = params[i].slice("")
+        
+        let iniziali = name[0]
+        
+        newArr.push(iniziali)
+        
+    }
+
+    return newArr
+    
+
+    // console.log(newArr);
+    
+}
+
+let newArr = selectName(names)
+
+console.log(newArr);
+
+
+// console.log(selectName(names, newArr));
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
